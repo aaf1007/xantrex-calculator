2797efb494f60dfe6048314ac495efb9e87c3239
erfan604 <erfanaghdasi78@gmail.com>
2026-03-08 17:13:47 -0700

    add MpptController entity, repo, and implemented DashboardController

--- src/main/java/com/group18/xantrex_calculator/controller/DashboardController.java
+++ src/main/java/com/group18/xantrex_calculator/controller/DashboardController.java
@@ -1,30 +1,37 @@
-  package com.group18.xantrex_calculator.controller;                            
-                                                                                
-  import org.springframework.stereotype.Controller;                             
-  import org.springframework.ui.Model;
-  import org.springframework.web.bind.annotation.*;
+package com.group18.xantrex_calculator.controller;
 
-  import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-  @Controller
-  @RequestMapping("/dashboard")
-  public class DashboardController {
+import com.group18.xantrex_calculator.repository.MpptControllerRepository;
 
-      @GetMapping
-      public String dashboard(Model model) {
-          model.addAttribute("controllers", new ArrayList<>());
-          return "dashboard";
-      }
+import com.group18.xantrex_calculator.entity.MpptController;
 
-      @PostMapping("/add")
-      public String addController() {
-          // TODO: After Antons work
-          return "redirect:/dashboard";
-      }
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+    private final MpptControllerRepository controllerRepository;
 
-      @PostMapping("/delete")
-      public String deleteController() {
-          // TODO: After Antons work
-          return "redirect:/dashboard";
-      }
-  }
+    public DashboardController(MpptControllerRepository controllerRepository) {
+        this.controllerRepository = controllerRepository;
+    }
+
+    @GetMapping
+    public String dashboard(Model model) {
+        model.addAttribute("controllers", controllerRepository.findAll());
+        return "dashboard";
+    }
+
+    @PostMapping("/add")
+    public String addController(@ModelAttribute MpptController controller) {
+        controllerRepository.save(controller);
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/delete")
+    public String deleteController(@RequestParam Long id) {
+        controllerRepository.deleteById(id);
+        return "redirect:/dashboard";
+    }
+}
--- src/main/java/com/group18/xantrex_calculator/repository/MpptControllerRepository.java
+++ src/main/java/com/group18/xantrex_calculator/repository/MpptControllerRepository.java
@@ -1,5 +1,7 @@
 package com.group18.xantrex_calculator.repository;
 
-public class MpptControllerRepository {
-    
+import com.group18.xantrex_calculator.entity.MpptController;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MpptControllerRepository extends JpaRepository<MpptController, Long> {
 }
