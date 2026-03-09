b75912b7a3f4dda324cc2de7360e5b80cf914229
Huda <hudaqazi2005@gmail.com>
2026-03-09 01:53:45 -0700

    Added CalculatorControllerTest.java

---
+++ src/test/java/com/group18/xantrex_calculator/controller/CalculatorControllerTest.java
@@ -0,0 +1,83 @@
+package com.group18.xantrex_calculator.controller;
+
+import com.group18.xantrex_calculator.model.CalculatorResult;
+import com.group18.xantrex_calculator.entity.MpptController;
+import com.group18.xantrex_calculator.service.CalculatorService;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CalculatorController.class)
+public class CalculatorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CalculatorService calculatorService;
+
+    @Test
+    void testCalculatorPage() throws Exception {
+
+        mockMvc.perform(get("/calculator"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("userdashboard"));
+    }
+
+    /* Test POST /calculator calculation*/
+    @Test
+    void testCalculate() throws Exception {
+
+        CalculatorResult result = new CalculatorResult(
+                3120.0,
+                142.8,
+                212.2,
+                20.0
+        );
+
+        MpptController controller = new MpptController();
+        controller.setId(1L);
+        controller.setBatteryBank("12V");
+        controller.setMaxVoc(150.0);
+        controller.setMaxCurrent(250.0);
+        controller.setMaxIsc(30.0);
+
+        when(calculatorService.calculate(
+                260, 23.8, 10.0, 6, 2, 12, 1.0))
+                .thenReturn(result);
+
+        when(calculatorService.findMatchingController(result, "12V"))
+                .thenReturn(Optional.of(controller));
+
+        mockMvc.perform(post("/calculator")
+                        .param("pmax", "260")
+                        .param("voc", "23.8")
+                        .param("isc", "10.0")
+                        .param("series", "6")
+                        .param("parallel", "2")
+                        .param("battV", "12")
+                        .param("tempFactor", "1.0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attributeExists("result"))
+                .andExpect(model().attributeExists("recommendedController"));
+
+        verify(calculatorService, times(1))
+                .calculate(260, 23.8, 10.0, 6, 2, 12, 1.0);
+
+        verify(calculatorService, times(1))
+                .findMatchingController(result, "12V");
+    }
+
+    /*Test when no controller matches*/
+}
